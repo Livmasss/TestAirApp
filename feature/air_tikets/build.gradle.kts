fe26cc3cf54c1b8f329c3303980902a408cc3220
livmas <seiranyan.sg@gmail.com>
2024-06-17 00:26:28 +0300

    music_recycler_item.xml created to be used in Music RecyclerView.

--- feature/air_tikets/build.gradle.kts
+++ feature/air_tikets/build.gradle.kts
@@ -39,6 +39,8 @@ android {
 dependencies {
     implementation(project(":ui"))
 
+    implementation("com.google.android.material:material:1.6.0")
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
